087860083477b0086cb8cc412e15a7cea02b990c
dnutsubidze <108520230+dnutsubidze@users.noreply.github.com>
2026-03-10 19:06:06 +0400

    cicd

--- BookingSystem.API/Program.cs
+++ BookingSystem.API/Program.cs
@@ -42,6 +42,12 @@ builder.Services.AddCors(options =>
 });
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 //დროებით
 //if (app.Environment.IsDevelopment())
 //{
